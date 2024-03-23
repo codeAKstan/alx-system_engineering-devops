68a8674a475031c9a9d8fafb246faf34dde70647
codeAKstan <boystanismoses@gmail.com>
2024-03-23 08:21:56 -0700

    Modified task 1, added install command for Werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-#!/usr/bin/pup
+#!/usr/bin/puppet
 # Install flask from pip3 using puppet
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
+
+package {'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
+  }
