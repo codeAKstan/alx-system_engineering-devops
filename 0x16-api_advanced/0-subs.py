b970f96c727fad7d83b3e2b1b17ddb71ae2b779d
codeAKstan <boystanismoses@gmail.com>
2024-05-07 02:34:17 -0700

    Added task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Function to get all subscribers"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """Return the number of subscribers for a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json().get("data")
+        return data.get("subscribers")
+    else:
+        return 0
