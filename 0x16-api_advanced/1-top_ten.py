155aed26313989f6ef1c124f13cc60a0ca936751
codeAKstan <boystanismoses@gmail.com>
2024-05-08 04:04:44 -0700

    Modified task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,22 +4,22 @@ a function that queries the Reddit API and prints
 the titles of the first 10 hot posts listed for a given subreddit.
 """
 import requests
+from sys import argv
 
 
 def top_ten(subreddit):
-    """Print titles of the first 10 hot posts for a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
-    params = {"limit": 10}
-    response = requests.get(url, headers=headers, params=params)
+    '''
+        returns the top ten posts for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
+    try:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
 
-    if response.status_code == 200:
-        data = response.json().get("data")
-        children = data.get("children")
-        if children:
-            for post in children:
-                print(post.get("data").get("title"))
-        else:
-            print("No posts found for the subreddit.")
-    else:
-        print("None")
+
+if __name__ == "__main__":
+    top_ten(argv[1])
